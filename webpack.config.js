c8e8cc9b5a9c84a97be02f36af71022339c6f7a2
Harsh Gautam <hgautam208@gmail.com>
2021-09-28 20:17:07 +0530

    added custom webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  devtool: "inline-source-map",
+  output: {
+    filename: "index.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
