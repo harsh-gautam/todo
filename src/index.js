f44b0fab638b57ed149ce29a2303f14657bc98c2
Harsh Gautam <hgautam208@gmail.com>
2021-10-17 17:40:23 +0530

    added modal to add new task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import { createButtonGroup, createWelcomeDiv, createInsightsDiv } from "./home";
 import createElement from "./htmlUtility";
 
+function showModal() {
+  const modal = document.querySelector("#id-modal-task");
+  modal.style.display = "block";
+}
+
+const createTask = document.querySelector("div.tasks-toolbar > button");
+createTask.addEventListener("click", showModal);
+
 function setupHome() {
   console.log("Setup");
   // Home section
