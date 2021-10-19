91b49462afcb8d828d494c45d2d9ed6c1e119055
Harsh Gautam <hgautam208@gmail.com>
2021-10-20 02:17:13 +0530

    restored utility.js

--- src/utility.js
+++ src/utility.js
@@ -9,8 +9,72 @@ function createElement(type, classes = [], id = "") {
   return element;
 }
 
-function createFragment() {
-  return document.createDocumentFragment();
+function createTask(title, dueDate, pr) {
+  // checkbox
+  const checkbox = createElement("input", [], "task-complete");
+  checkbox.name = "task-complete";
+  checkbox.type = "checkbox";
+
+  // title
+  const titleElem = createElement("p", ["task-heading"]);
+
+  if (title.length > 18) title = title.slice(0, 17) + "...";
+  titleElem.textContent = title;
+
+  // time stamp
+  const timeElem = createElement("p", ["time-stamp"]);
+  timeElem.textContent = dueDate;
+
+  // edit button
+  const editElem = createElement("span");
+  const iEdit = createElement("i", ["bi", "bi-pencil-square"]);
+  editElem.appendChild(iEdit);
+
+  //delete button
+  const delElem = createElement("span");
+  const iDel = createElement("i", ["bi", "bi-trash"]);
+  delElem.appendChild(iDel);
+
+  const wrapper = createElement("div", ["task"]);
+  wrapper.dataset.priority = pr;
+  wrapper.append(checkbox, titleElem, timeElem, editElem, delElem);
+
+  return wrapper;
 }
 
+function convertDate(date) {
+  // date : yyyy-mm-dd, eg-> 2021-10-24
+  // output: 24th Oct
+
+  const months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let [year, month, day] = date.split("-");
+  month = months[month - 1].slice(0, 3);
+
+  if (day.slice(-1) == "1") {
+    day = day + "st";
+  } else if (day.slice(-1) == "2") {
+    day = day + "nd";
+  } else if (day.slice(-1) == "3") {
+    day = day + "rd";
+  } else {
+    day = day + "th";
+  }
+  return `${day} ${month}`;
+}
+
+export { createTask, convertDate };
 export default createElement;
